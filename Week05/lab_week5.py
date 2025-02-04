48aaa3c755de977f8d608020bf3c7c50cd9af607
HamiLia <cvsd1144531@gmail.com>
2025-02-04 10:53:33 -0500

    add -1 solution

--- Week05/lab_week5.py
+++ Week05/lab_week5.py
@@ -224,6 +224,19 @@ if not input_invalid:
         print("    |", end="    ")
 
         # Lab 5 - Question 1: 
+        hero_name = input("Enter hero's name (in two words): ")
+        name = hero_name.split()
+        if len(name) != 2:
+            print("    |    Pleas enter a name with two parts(separated by a space)")
+            tries += 1
+        else:
+            if not name[0].isalpha() + name[1].isalpha:
+                print("    |    Please enter an alphabetical name")
+                tries += 1
+            else:
+                short_name = name[0][0:2:1] + name[0][0:1:1]
+                print("    |    I'm going to call you " + short_name + " for short")
+                input_invalid = False    
         
 
     if not input_invalid:
